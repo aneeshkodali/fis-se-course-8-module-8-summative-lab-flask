5e6ce42d4785faefb2fa0eda7556ef62d2ba3e07
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-03-15 17:48:29 +0100

    Redo api functions

--- utils/open_food_facts.py
+++ utils/open_food_facts.py
@@ -3,41 +3,73 @@ import requests
 
 # constants
 OPEN_FOOD_FACTS_BASE_URL = 'https://world.openfoodfacts.org'
+OPEN_FOOD_FACTS_FIELD_LIST = [
+    'id', 'product_name', 'product_type', 'quantity',
+] # assumes these fields that are to be returned are FIXED
+
+# create variables from constants
+OPEN_FOOD_FACTS_FIELD_LIST_CONCAT = ','.join(OPEN_FOOD_FACTS_FIELD_LIST)
 
 
-# search api for product
+# search api for product based on name
 def search_product_name(product_name):
     '''
     Retrieves product from api
     - Uses search endpoint
-    - Field retrieved are fixed
     - Returns first result from response list
     '''
 
-    # construct request properties
-    endpoint = '/api/v2/search' # search endpoint
-    fields = [
-        'id', 'product_name', 'product_type', 'quantity',
-
-    ] # list of fields to return
-    fields_concat = ','.join(fields) # concatenate for request parameter
-
     # construct request parameter
+    endpoint = '/cgi/search.pl'
+    api_url = f"{OPEN_FOOD_FACTS_BASE_URL}{endpoint}"
     params = {
-        'product_name': product_name,
-        'fields': fields_concat,
+        'search_terms': product_name,
+        'search_simple': 1,
+        'action': 'process',
+        'json': 1,
+        'fields': OPEN_FOOD_FACTS_FIELD_LIST_CONCAT,
     }
 
     # make request
-    response = requests.get(url=f"{OPEN_FOOD_FACTS_BASE_URL}{endpoint}", params=params)
+    response = requests.get(url=api_url, params=params)
     response.raise_for_status()
-    
-    # parse response
     data = response.json()
     products = data.get('products', [])
 
     # return product (if exists)
-    if not products:
-        print(f"No product `{product_name}` found.")
+    product = products[0] if products else None
+    if not product:
+        print(f"No product with name `{product_name}` found.")
         return {}
-    return products[:1]
+    return product
+
+# search api for product based on id
+def search_product_id(product_id):
+    '''
+    Retrieves product from api
+    - Uses search endpoint
+    - Returns first result from response list
+    - Assumes that barcode and id are identical
+    '''
+
+    # construct request parameter
+    endpoint = f"/api/v2/product/{product_id}.json"
+    api_url = f"{OPEN_FOOD_FACTS_BASE_URL}{endpoint}"
+    params = {
+        'fields': OPEN_FOOD_FACTS_FIELD_LIST_CONCAT,
+    }
+
+    # make request
+    response = requests.get(url=api_url, params=params)
+
+    if response.status_code == 404:
+        print(f"No product with ID {product_id} found.")
+        return {}
+    
+    response.raise_for_status()
+
+    # parse product from response
+    data = response.json()
+    product = data.get('product', {})
+    
+    return product
