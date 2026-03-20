d70d8b818b085998ebc78fad2e4510edda18410d
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-03-20 16:44:30 -0400

    Add PATCH endpoint

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 # imports
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from utils.open_food_facts import search_product
 from utils.storage import load_data, save_data
-import requests
 
 # initialize flask app
 app = Flask(__name__)
@@ -40,6 +39,7 @@ def get_inventory_by_id(id):
         None
     )
 
+    # exit if no item found
     if not inventory_item:
         return jsonify({"error": f"Inventory item with ID {id} not found"}), 404 # return error if not found
     
@@ -49,10 +49,10 @@ def get_inventory_by_id(id):
 @app.route('/inventory', methods=['POST'])
 def create_inventory():
 
-    # take incoming data
-    data = requests.get_json()
+    # get incoming request data
+    data = request.get_json()
 
-    # exit if not product in data
+    # exit if not product in request data
     if not data.get('product'):
         return jsonify({'message': 'Product not found in request'}), 400
     
@@ -75,6 +75,41 @@ def create_inventory():
     # save and return inventory
     save_data(file_path=INVENTORY_FILE, data=inventory_data)
     return jsonify(new_inventory), 201
+
+# update inventory item
+@app.route('/inventory/<int:id>', methods=['PATCH'])
+def update_inventory(id):
+
+    # get incoming request data
+    data = request.get_json()
+
+    # read in inventory data
+    inventory_data = load_data(file_path=INVENTORY_FILE)
+
+    # set variable to keep track of update
+    update_bool = False
+
+    # loop through inventory data
+    for inventory_item in inventory_data:
+
+        # look for item based on id
+        if inventory_item['id'] == id:
+
+            # update item
+            inventory_item = {
+                **inventory_item,
+                **data, # make sure data is 2nd so it's data overwrites prior data
+            }
+            # break out of loop
+            break
+
+    if not update_bool:
+        # exit if no item found
+        return jsonify({"error": f"Inventory item with ID {id} not found"}), 404 # return error if not found
+    
+    # save data
+    save_data(file_path=INVENTORY_FILE, data=inventory_data)
+    return jsonify(inventory_item), 200
 
 
 if __name__ == '__main__':
